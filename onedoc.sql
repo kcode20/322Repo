cd75d02d879eb493bd1d6c1616fc793a6f0d88b3
connie-code <conniewu06@gmail.com>
2018-12-10 23:52:37 -0500

    Updated + revised database file

--- onedoc.sql
+++ onedoc.sql
@@ -1,89 +1,25 @@
--- MySQL dump 10.13  Distrib 5.5.62, for debian-linux-gnu (x86_64)
---
--- Host: 35.202.162.146    Database: onedocapp
--- ------------------------------------------------------
--- Server version	5.7.14-google-log
+DROP DATABASE onedoc;
+CREATE DATABASE onedoc;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE onedoc;
 
---
--- Table structure for table `Collaborators`
---
+DROP TABLE IF EXISTS `Users`;
 
-DROP TABLE IF EXISTS `Collaborators`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Collaborators` (
+CREATE TABLE `Users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `docID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `classID` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `docID` (`docID`),
-  KEY `userID` (`userID`),
-  KEY `classID` (`classID`),
-  CONSTRAINT `Collaborators_ibfk_1` FOREIGN KEY (`docID`) REFERENCES `Documents` (`docID`),
-  CONSTRAINT `Collaborators_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `Users` (`id`),
-  CONSTRAINT `Collaborators_ibfk_3` FOREIGN KEY (`classID`) REFERENCES `Permissions` (`classID`)
+  `Username` varchar(15) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` char(60) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `Collaborators`
---
+LOCK TABLES `Users` WRITE;
 
-LOCK TABLES `Collaborators` WRITE;
-/*!40000 ALTER TABLE `Collaborators` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Collaborators` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `Complaints`
---
-
-DROP TABLE IF EXISTS `Complaints`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Complaints` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `docID` int(11) NOT NULL,
-  `Author` int(11) NOT NULL,
-  `Issue` varchar(60) NOT NULL,
-  `Created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `Resolved` bit(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `Author` (`Author`),
-  KEY `docID` (`docID`),
-  CONSTRAINT `Complaints_ibfk_1` FOREIGN KEY (`Author`) REFERENCES `Collaborators` (`userID`),
-  CONSTRAINT `Complaints_ibfk_2` FOREIGN KEY (`docID`) REFERENCES `Collaborators` (`docID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Complaints`
---
-
-LOCK TABLES `Complaints` WRITE;
-/*!40000 ALTER TABLE `Complaints` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Complaints` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Documents`
---
 
 DROP TABLE IF EXISTS `Documents`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Documents` (
   `docID` int(11) NOT NULL AUTO_INCREMENT,
   `Owner` int(11) NOT NULL,
@@ -96,24 +32,14 @@ CREATE TABLE `Documents` (
   KEY `Owner` (`Owner`),
   CONSTRAINT `Documents_ibfk_1` FOREIGN KEY (`Owner`) REFERENCES `Users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `Documents`
---
 
 LOCK TABLES `Documents` WRITE;
-/*!40000 ALTER TABLE `Documents` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Documents` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `Permissions`
---
-
 DROP TABLE IF EXISTS `Permissions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Permissions` (
   `classID` int(11) NOT NULL AUTO_INCREMENT,
   `Class` tinytext NOT NULL,
@@ -128,24 +54,54 @@ CREATE TABLE `Permissions` (
   `Comment` bit(1) NOT NULL,
   PRIMARY KEY (`classID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Permissions`
---
 
 LOCK TABLES `Permissions` WRITE;
-/*!40000 ALTER TABLE `Permissions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Permissions` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `Revisions`
---
+DROP TABLE IF EXISTS `Collaborators`;
+
+CREATE TABLE `Collaborators` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `docID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `classID` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `docID` (`docID`),
+  KEY `userID` (`userID`),
+  KEY `classID` (`classID`),
+  CONSTRAINT `Collaborators_ibfk_1` FOREIGN KEY (`docID`) REFERENCES `Documents` (`docID`),
+  CONSTRAINT `Collaborators_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `Users` (`id`),
+  CONSTRAINT `Collaborators_ibfk_3` FOREIGN KEY (`classID`) REFERENCES `Permissions` (`classID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `Collaborators` WRITE;
+
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `Complaints`;
+
+CREATE TABLE `Complaints` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `docID` int(11) NOT NULL,
+  `Author` int(11) NOT NULL,
+  `Issue` varchar(60) NOT NULL,
+  `Created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Resolved` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Author` (`Author`),
+  KEY `docID` (`docID`),
+  CONSTRAINT `Complaints_ibfk_1` FOREIGN KEY (`Author`) REFERENCES `Collaborators` (`userID`),
+  CONSTRAINT `Complaints_ibfk_2` FOREIGN KEY (`docID`) REFERENCES `Collaborators` (`docID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `Complaints` WRITE;
+
+UNLOCK TABLES;
+
 
 DROP TABLE IF EXISTS `Revisions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Revisions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `docID` int(11) NOT NULL,
@@ -160,49 +116,7 @@ CREATE TABLE `Revisions` (
   CONSTRAINT `Revisions_ibfk_1` FOREIGN KEY (`Author`) REFERENCES `Collaborators` (`userID`),
   CONSTRAINT `Revisions_ibfk_2` FOREIGN KEY (`docID`) REFERENCES `Collaborators` (`docID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Revisions`
---
 
 LOCK TABLES `Revisions` WRITE;
-/*!40000 ALTER TABLE `Revisions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Revisions` ENABLE KEYS */;
+
 UNLOCK TABLES;
-
---
--- Table structure for table `Users`
---
-
-DROP TABLE IF EXISTS `Users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `Username` varchar(15) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Password` char(60) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Users`
---
-
-LOCK TABLES `Users` WRITE;
-/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-12-10 20:07:34
