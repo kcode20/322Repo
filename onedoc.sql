029b972634535f358b61fef18f7207c9f5f12f46
connie-code <conniewu06@gmail.com>
2018-12-11 00:29:25 -0500

    Revised database sql file

--- onedoc.sql
+++ onedoc.sql
@@ -1,9 +1,9 @@
 DROP DATABASE onedoc;
 CREATE DATABASE onedoc;
 
-USE onedoc;
+USE onedoc; -- use onedoc database
 
-DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE `Users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -13,110 +13,102 @@ CREATE TABLE `Users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `Users` WRITE;
-
+LOCK TABLES users WRITE;
 UNLOCK TABLES;
 
+--
+DROP TABLE IF EXISTS documents;
 
-DROP TABLE IF EXISTS `Documents`;
-
-CREATE TABLE `Documents` (
-  `docID` int(11) NOT NULL AUTO_INCREMENT,
-  `Owner` int(11) NOT NULL,
-  `Title` varchar(45) NOT NULL,
-  `Content` blob,
-  `Created_at` datetime NOT NULL,
-  `Modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `Locked` bit(1) NOT NULL,
-  PRIMARY KEY (`docID`),
-  KEY `Owner` (`Owner`),
-  CONSTRAINT `Documents_ibfk_1` FOREIGN KEY (`Owner`) REFERENCES `Users` (`id`)
+CREATE TABLE documents (
+  docID int(11) NOT NULL AUTO_INCREMENT,
+  owner int(11) NOT NULL,
+  title varchar(45) NOT NULL,
+  content blob,
+  created_at datetime NOT NULL,
+  modified_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  locked bit(1) NOT NULL,
+  PRIMARY KEY (docID),
+  KEY owner (owner),
+  FOREIGN KEY (owner) REFERENCES users (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-LOCK TABLES `Documents` WRITE;
-
+LOCK TABLES documents WRITE;
 UNLOCK TABLES;
+--
 
-DROP TABLE IF EXISTS `Permissions`;
+DROP TABLE IF EXISTS permissions;
 
-CREATE TABLE `Permissions` (
-  `classID` int(11) NOT NULL AUTO_INCREMENT,
-  `Class` tinytext NOT NULL,
-  `Wrt` bit(1) NOT NULL,
-  `R` bit(1) NOT NULL,
-  `Complain` bit(1) NOT NULL,
-  `New` bit(1) NOT NULL,
-  `Share` bit(1) NOT NULL,
-  `Loc` bit(1) NOT NULL,
-  `Rec` bit(1) NOT NULL,
-  `Download` bit(1) NOT NULL,
-  `Comment` bit(1) NOT NULL,
-  PRIMARY KEY (`classID`)
+CREATE TABLE permissions (
+  classID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  class tinytext NOT NULL,
+  wrt bit(1) NOT NULL,
+  R bit(1) NOT NULL,
+  complain bit(1) NOT NULL,
+  new bit(1) NOT NULL,
+  share bit(1) NOT NULL,
+  loc bit(1) NOT NULL, -- lock
+  rec bit(1) NOT NULL,  -- record if a user can revise, record changes
+  download bit(1) NOT NULL,
+  comment bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `Permissions` WRITE;
-
+LOCK TABLES permissions WRITE;
 UNLOCK TABLES;
+--
 
-DROP TABLE IF EXISTS `Collaborators`;
+DROP TABLE IF EXISTS collaborators;
 
-CREATE TABLE `Collaborators` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `docID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `classID` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `docID` (`docID`),
-  KEY `userID` (`userID`),
-  KEY `classID` (`classID`),
-  CONSTRAINT `Collaborators_ibfk_1` FOREIGN KEY (`docID`) REFERENCES `Documents` (`docID`),
-  CONSTRAINT `Collaborators_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `Users` (`id`),
-  CONSTRAINT `Collaborators_ibfk_3` FOREIGN KEY (`classID`) REFERENCES `Permissions` (`classID`)
+CREATE TABLE collaborators (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  docID int(11) NOT NULL,
+  userID int(11) NOT NULL,
+  classID int(11) NOT NULL,
+  KEY docID (docID),
+  KEY userID (userID),
+  KEY classID (classID),
+  FOREIGN KEY (docID) REFERENCES documents (docID),
+  FOREIGN KEY (userID) REFERENCES users (id),
+  FOREIGN KEY (classID) REFERENCES permissions (classID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `Collaborators` WRITE;
-
+LOCK TABLES collaborators WRITE;
 UNLOCK TABLES;
+--
 
-DROP TABLE IF EXISTS `Complaints`;
+DROP TABLE IF EXISTS complaints;
 
-CREATE TABLE `Complaints` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `docID` int(11) NOT NULL,
-  `Author` int(11) NOT NULL,
-  `Issue` varchar(60) NOT NULL,
-  `Created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `Resolved` bit(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `Author` (`Author`),
-  KEY `docID` (`docID`),
-  CONSTRAINT `Complaints_ibfk_1` FOREIGN KEY (`Author`) REFERENCES `Collaborators` (`userID`),
-  CONSTRAINT `Complaints_ibfk_2` FOREIGN KEY (`docID`) REFERENCES `Collaborators` (`docID`)
+CREATE TABLE complaints (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  docID int(11) NOT NULL,
+  author int(11) NOT NULL,
+  issue varchar(60) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  resolved bit(1) NOT NULL,
+  KEY author (author),
+  KEY docID (docID),
+  FOREIGN KEY (author) REFERENCES collaborators (userID),
+  FOREIGN KEY (docID) REFERENCES collaborators (docID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `Complaints` WRITE;
-
+LOCK TABLES complaints WRITE;
 UNLOCK TABLES;
+--
 
+DROP TABLE IF EXISTS revisions;
 
-DROP TABLE IF EXISTS `Revisions`;
-
-CREATE TABLE `Revisions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `docID` int(11) NOT NULL,
-  `Author` int(11) NOT NULL,
-  `Type` tinytext NOT NULL,
-  `Revised` blob NOT NULL,
-  `Created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `Accept` bit(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `Author` (`Author`),
-  KEY `docID` (`docID`),
-  CONSTRAINT `Revisions_ibfk_1` FOREIGN KEY (`Author`) REFERENCES `Collaborators` (`userID`),
-  CONSTRAINT `Revisions_ibfk_2` FOREIGN KEY (`docID`) REFERENCES `Collaborators` (`docID`)
+CREATE TABLE revisions (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  docID int(11) NOT NULL,
+  author int(11) NOT NULL,
+  type tinytext NOT NULL,
+  revised blob NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  accept bit(1) DEFAULT NULL,
+  KEY author (author),
+  KEY docID (docID),
+  FOREIGN KEY (author) REFERENCES collaborators (userID),
+  FOREIGN KEY (docID) REFERENCES collaborators (docID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOCK TABLES `Revisions` WRITE;
-
+LOCK TABLES revisions WRITE;
 UNLOCK TABLES;
