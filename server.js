ea1defaa78c050d90ae133c845fd00c5961eaf83
Khristian Brooks <kh.brooks02@gmail.com>
2018-12-11 02:40:19 -0500

    fixed cors error, pass body

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 const express = require('express');
 const mysql = require('mysql');
-const Parser = require("body-parser");
+const Parser = require('body-parser');
 const app = express();
+var cors = require('cors');
 
-app.use(express.static(__dirname + "/public"));
-app.use(Parser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(Parser.urlencoded({ extended: true }));
 app.use(Parser.json());
-
+app.use(cors());
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    // password: 'yourDataBasePassword',
-    password: "Noosa11",
-    database: 'onedoc'
+	host: 'localhost',
+	user: 'root',
+	// password: 'yourDataBasePassword',
+	password: 'Noosa11',
+	database: 'onedoc',
 });
 
-app.post("/signup", function(req, res){
-  console.log(req);
-  let userName = req.body.username;
-  let email = req.body.email;
-  let password = req.body.password;
-  // let q = `INSERT INTO users(username, email, password) VALUES (${userName}, ${email}, ${password})`;
-  let q = "INSERT INTO users(username, email, password) VALUES ('" + userName + "', '" + email + "', '" + password + "')";
-  console.log(q);
-  connection.query(q, function(err, results){
-    if(err) throw err;
-  });
-  res.redirect("/document");
+app.post('/signup', function(req, res) {
+	res.header('Access-Control-Allow-Origin', '*');
+
+	console.log(req.body, req.headers);
+	let userName = req.body.username;
+	let email = req.body.email;
+	let password = req.body.password;
+	let q = `INSERT INTO users(username, email, password) VALUES ('${userName}', '${email}', '${password})'`;
+	console.log(q);
+	connection.query(q, function(err, results) {
+		if (err) throw err;
+	});
+	res.redirect('/document');
 });
 
-app.listen(8080, function(){
-  console.log("Server running on 8080");
+app.listen(8080, function() {
+	console.log('Server running on 8080');
 });
--- src/views/signupin/pages/SignUpForm.jsx
+++ src/views/signupin/pages/SignUpForm.jsx
@@ -1,112 +1,183 @@
 import React, { Component } from 'react';
-import { Link, NavLink  } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import '../login.css';
 
 class SignUpForm extends Component {
-    constructor() {
-        super();
+	constructor() {
+		super();
 
-        this.state = {
-            username: "",
-            email: '',
-            password: '',
-            name: '',
-            hasAgreed: false
-        };
+		this.state = {
+			username: '',
+			email: '',
+			password: '',
+			name: '',
+			hasAgreed: false,
+		};
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-    handleChange(e) {
-        let target = e.target;
-        let value = target.type === 'checkbox' ? target.checked : target.value;
-        let name = target.name;
+	handleChange(e) {
+		let target = e.target;
+		let value = target.type === 'checkbox' ? target.checked : target.value;
+		let name = target.name;
 
-        this.setState({
-          [name]: value
-        });
-    }
+		this.setState({
+			[name]: value,
+		});
+	}
 
-    handleSubmit(e) {
-        e.preventDefault();
-        var data = {
-          username: this.state.username,
-          email: this.state.email,
-          password: this.state.password,
-        }
-        console.log(data);
-        fetch('http://localhost:8080/signup',{
-          method: 'POST',
-          mode: 'no-cors',
-          headers: {
-            'Access-Control-Allow-Origin':'*',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-        }).then(function(response) {
-            if (response.status >= 400) {
-              throw new Error("Bad response from server");
-            }
-            return response.json();
-        }).then(function(data) {
-            console.log(data)
-            if(data === "success"){
-               this.setState({msg: "Thanks for registering"});
-            }
-        }).catch(function(err) {
-            console.log(err)
-        });
-        console.log('The form was submitted with the following data:');
-        console.log(this.state);
+	handleSubmit(e) {
+		e.preventDefault();
+		var data = {
+			username: this.state.username,
+			email: this.state.email,
+			password: this.state.password,
+		};
+		console.log(JSON.stringify(data));
+		fetch('http://localhost:8080/signup', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+				'Access-Control-Allow-Origin': '*',
+			},
+			body: JSON.stringify(data),
+		})
+			.then(function(response) {
+				if (response.status >= 400) {
+					throw new Error('Bad response from server');
+				}
+				return response.json();
+			})
+			.then(function(data) {
+				console.log(data);
+				if (data === 'success') {
+					this.setState({ msg: 'Thanks for registering' });
+				}
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+		console.log('The form was submitted with the following data:');
+		console.log(this.state);
+	}
 
+	render() {
+		return (
+			<div className="App">
+				<div className="App__Aside" />
+				<div className="App__Form">
+					<div className="PageSwitcher">
+						<NavLink
+							to="/signin"
+							activeClassName="PageSwitcher__Item--Active"
+							className="PageSwitcher__Item"
+						>
+							Sign In
+						</NavLink>
+						<NavLink
+							exact
+							to="/signup"
+							activeClassName="PageSwitcher__Item--Active"
+							className="PageSwitcher__Item"
+						>
+							Sign Up
+						</NavLink>
+					</div>
 
-    }
+					<div className="FormTitle">
+						<NavLink
+							to="/signin"
+							activeClassName="FormTitle__Link--Active"
+							className="FormTitle__Link"
+						>
+							Sign In
+						</NavLink>
+						or
+						<NavLink
+							exact
+							to="/signup"
+							activeClassName="FormTitle__Link--Active"
+							className="FormTitle__Link"
+						>
+							Sign Up
+						</NavLink>
+					</div>
+					<div className="FormCenter">
+						<form onSubmit={this.handleSubmit} className="FormFields">
+							<div className="FormField">
+								<label className="FormField__Label" htmlFor="name">
+									Username
+								</label>
+								<input
+									type="text"
+									id="username"
+									className="FormField__Input"
+									placeholder="Enter your username"
+									name="username"
+									value={this.state.username}
+									onChange={this.handleChange}
+								/>
+							</div>
+							<div className="FormField">
+								<label className="FormField__Label" htmlFor="password">
+									Password
+								</label>
+								<input
+									type="password"
+									id="password"
+									className="FormField__Input"
+									placeholder="Enter your password"
+									name="password"
+									value={this.state.password}
+									onChange={this.handleChange}
+								/>
+							</div>
+							<div className="FormField">
+								<label className="FormField__Label" htmlFor="email">
+									E-Mail Address
+								</label>
+								<input
+									type="email"
+									id="email"
+									className="FormField__Input"
+									placeholder="Enter your email"
+									name="email"
+									value={this.state.email}
+									onChange={this.handleChange}
+								/>
+							</div>
 
-    render() {
-        return (
-            <div className="App">
-          <div className="App__Aside"></div>
-          <div className="App__Form">
-            <div className="PageSwitcher">
-                <NavLink to="/signin" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-                <NavLink exact to="/signup" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
-            </div>
+							<div className="FormField">
+								<label className="FormField__CheckboxLabel">
+									<input
+										className="FormField__Checkbox"
+										type="checkbox"
+										name="hasAgreed"
+										value={this.state.hasAgreed}
+										onChange={this.handleChange}
+									/>{' '}
+									I agree all statements in{' '}
+									<a href="" className="FormField__TermsLink">
+										terms of service
+									</a>
+								</label>
+							</div>
 
-            <div className="FormTitle">
-                <NavLink to="/signin" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink>
-                or
-                <NavLink exact to="/signup" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
-            </div>
-            <div className="FormCenter">
-            <form onSubmit={this.handleSubmit} className="FormFields">
-            <div className="FormField">
-                <label className="FormField__Label" htmlFor="name">Username</label>
-                <input type="text" id="username" className="FormField__Input" placeholder="Enter your username" name="username" value={this.state.username} onChange={this.handleChange} />
-              </div>
-              <div className="FormField">
-                <label className="FormField__Label" htmlFor="password">Password</label>
-                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
-              </div>
-              <div className="FormField">
-                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
-              </div>
-
-              <div className="FormField">
-                <label className="FormField__CheckboxLabel">
-                    <input className="FormField__Checkbox" type="checkbox" name="hasAgreed" value={this.state.hasAgreed} onChange={this.handleChange} /> I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
-                </label>
-              </div>
-
-              <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign Up</button> <Link to="/signin" className="FormField__Link">I'm already member</Link>
-              </div>
-            </form>
-          </div>
-          </div>
-        </div>
-        );
-    }
+							<div className="FormField">
+								<button className="FormField__Button mr-20">Sign Up</button>{' '}
+								<Link to="/signin" className="FormField__Link">
+									I'm already member
+								</Link>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 export default SignUpForm;
