353f6501dc65513ba8b78c1520999e7d4914868d
Songren Zhao <songrenzhao@gmail.com>
2018-12-12 13:20:03 -0500

    added search files

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import TabooWord from './views/tabooWord/TabooWord'
 import OUApplication from './views/applications/OUApplication'
 import ProcessComplain from './views/ProcessComplain/ProcessComplain'
 import docInvitation from './views/docInvitation/docInvitation'
+import searchFiles from './views/searchFiles/SearchFiles'
 const About = () => <h2>About</h2>;
 // const Users = () => <h2>Users</h2>; // Added users page
 
@@ -25,6 +26,7 @@ const AppRouter = () => (
 			<Route path="/signin" component={SignInForm} />
 			{/* <Route path="/complain" component={Complain} /> */}
 			<Route path="/docInvitation" component={docInvitation} />
+			<Route path="/searchfiles" component={searchFiles} />
 			<Route path="/tabooword" component={TabooWord} />
 			<Route path="/accordenyinv" component={AccDenyInvitation} />
 			<Route path="/OUApplication" component={OUApplication} />
