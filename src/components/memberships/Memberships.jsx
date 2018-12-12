6c83fe098d5db6e28f080a83e41bea3cf8a78333
Khristian Brooks <kh.brooks02@gmail.com>
2018-12-12 01:51:08 -0500

    add users table

--- src/components/memberships/Memberships.jsx
+++ src/components/memberships/Memberships.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Memberships.css';
+import { Table, Button } from 'reactstrap';
 
 class Memberships extends Component {
 	constructor() {
@@ -32,11 +33,52 @@ class Memberships extends Component {
 				console.log(err);
 			});
 	};
+
+	getUserType = type => {
+		if (type === 'G') return 'Guest';
+		if (type === 'SU') return 'Super User';
+		if (type === 'OU') return 'Ordinary User';
+	};
 	render() {
 		console.log(this.state.users);
 		return (
 			<div className="memberships">
 				<h1> Users </h1>
+				<Table>
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Username</th>
+							<th>Email</th>
+							<th>Type</th>
+							<th>Promote</th>
+							<th>Demote</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.users.map((user, key) => {
+							const type = this.getUserType(user.type);
+							return (
+								<tr key={key}>
+									<th scope="row">{user.id}</th>
+									<td>{user.username}</td>
+									<td>{user.email}</td>
+									<td>{type}</td>
+									<td>
+										<Button color="primary" size="sm">
+											Promote
+										</Button>
+									</td>
+									<td>
+										<Button color="primary" size="sm">
+											Demote
+										</Button>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</Table>
 			</div>
 		);
 	}
