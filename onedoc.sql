0a690cc3895937261a3bcf862c9a1d5a99671892
Chantelle Levy <chanie01234@users.noreply.github.com>
2018-12-11 21:50:21 -0500

    Update onedoc.sql

--- onedoc.sql
+++ onedoc.sql
@@ -8,6 +8,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username varchar(15) NOT NULL,
+  type varchar(15) NOT NULL DEFAULT 'Basic',
   email varchar(50) NOT NULL,
   password char(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -25,7 +26,7 @@ CREATE TABLE documents (
   content text,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   modified_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  locked bit(1) NOT NULL,
+  locked tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (docID),
   KEY owner (owner),
   FOREIGN KEY (owner) REFERENCES users (id)
@@ -41,14 +42,13 @@ CREATE TABLE permissions (
   classID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   class tinytext NOT NULL,
   wrt bit(1) NOT NULL,
-  R bit(1) NOT NULL,
-  complain bit(1) NOT NULL,
-  new bit(1) NOT NULL,
-  share bit(1) NOT NULL,
-  loc bit(1) NOT NULL, -- lock
-  rec bit(1) NOT NULL,  -- record if a user can revise, record changes
-  download bit(1) NOT NULL,
-  comment bit(1) NOT NULL
+  R tinyint(1) NOT NULL,
+  complain tinyint(1) NOT NULL,
+  share tinyint(1) NOT NULL,
+  loc tinyint(1) NOT NULL, -- lock
+  rec tinyint(1) NOT NULL,  -- record if a user can revise, record changes
+  download tinyint(1) NOT NULL,
+  comment tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES permissions WRITE;
@@ -82,7 +82,7 @@ CREATE TABLE complaints (
   author int(11) NOT NULL,
   issue varchar(60) NOT NULL,
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  resolved bit(1) NOT NULL,
+  resolved tinyint(1) NOT NULL,
   KEY author (author),
   KEY docID (docID),
   FOREIGN KEY (author) REFERENCES collaborators (userID),
@@ -102,7 +102,7 @@ CREATE TABLE revisions (
   type tinytext NOT NULL,
   revised blob NOT NULL,
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  accept bit(1) DEFAULT NULL,
+  accept tinyint(1) DEFAULT NULL,
   KEY author (author),
   KEY docID (docID),
   FOREIGN KEY (author) REFERENCES collaborators (userID),
@@ -111,13 +111,39 @@ CREATE TABLE revisions (
 
 LOCK TABLES revisions WRITE;
 UNLOCK TABLES;
+--
 
+DROP TABLE IF EXISTS `Taboo`;
+
+CREATE TABLE `Taboo` (
+  `tabooID` int(11) NOT NULL AUTO_INCREMENT,
+  `Bad_Words` tinytext NOT NULL,
+  PRIMARY KEY (`tabooID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `Taboo` WRITE;
+
+UNLOCK TABLES;
+--                           
+DROP TABLE IF EXISTS `Invitations`;
+
+CREATE TABLE `Invitations` (
+  `inviteID` int(11) NOT NULL AUTO_INCREMENT,
+  `Sendr` tinytext,
+  `docID` int(11) DEFAULT NULL,
+  `Receivr` tinytext,
+  PRIMARY KEY (`inviteID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `Invitations` WRITE;
+
+UNLOCK TABLES;
 
 -- Populate
 INSERT INTO users(username, email, password)
 VALUES ("Connie", "connie@gmail.com", "people123"),
   ("Friend", "friend@gmail.com", "people123");
-
+INSERT INTO `Taboo` VALUES (1,'ass'),(2,'asshole'),(3,'bastard'),(4,'crap'),(5,'Christ on a bike'),(6,'Christ on a cracker'),(7,'damn'),(8,'goddamn'),(9,'goddamnit'),(10,'hell'),(11,'shit'),(12,'holyshit'),(13,'Jesus Christ'),(14,'Jesus'),(15,'shit'),(16,'whore'),(17,'stupid'),(18,'millenials'),(19,'dummy'),(20,'Bloody Hell'),(21,'Rubbish');
 INSERT INTO documents(owner, title, content, locked) VALUES('1', 'My First Document', 'This is my first document. I love to write!', 0);
 INSERT INTO documents(owner, title, content, locked) VALUES('1', 'My Second Document', 'This is my second document. I like to write!', 0);
 INSERT INTO documents(owner, title, content, locked) VALUES('1', 'My Third Document', 'This is my third document. I hate to write!', 0);
