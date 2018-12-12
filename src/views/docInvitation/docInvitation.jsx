5c959067132d44777d0da6e584c64492429556c6
Chantelle Levy <clevy000@gmail.com>
2018-12-12 17:00:41 -0500

    fixed the docInvitation page and added Back Button

--- src/views/docInvitation/docInvitation.jsx
+++ src/views/docInvitation/docInvitation.jsx
@@ -5,17 +5,13 @@ export default class docInvitation extends React.Component{
     constructor(){
         super();
         this.state = {
-            documentID: "",
-            inputUsername1: "",
-            inputUsername2: "",
+            inputUsername: "",
         }
     }
 
     changeHandler = (event) => {
         this.setState({
-            documentID: event.target.value,
-            inputUsername1: event.target.value,
-            inputUsername2: event.target.value,
+            inputUsername: event.target.value,
         });
     }
 
@@ -23,27 +19,21 @@ export default class docInvitation extends React.Component{
         //this.props.DocID and this.props.ownerID are needed to proceed...
         console.log("you will be submitting the username below...");
         //Will submit this.props.DocID and this.props.ownerID and inputUsername...
-        console.log(this.state.documentID);
-        console.log(this.state.inputUsername1);
-        console.log(this.state.inputUsername2);
+        console.log(this.state.inputUsername);
 
         //Clearing the state, ready for more inputs...
         this.setState({
-            documentID:"",
-            inputUsername1: "",
-            inputUsername2: "",
+            inputUsername: "",
         })
     }
     render(){
         return(
             <div className = "form">
-                <input id = "documentID" type = "text" placeholder="document ID"
-                  value = {this.state.documentID} onChange = {(e) => this.changeHandler(e)}></input>
-                <input id = "inputUsername1" type = "text" placeholder="sender"
-                  value = {this.state.inputUsername1} onChange = {(e) => this.changeHandler(e)}></input>
-                <input id = "inputUsername2" type = "text" placeholder="receiver"
-                  value = {this.state.inputUsername2} onChange = {(e) => this.changeHandler(e)}></input>
-                <button onClick = {this.handleSubmit}>Submit</button>
+            <input id = "inputUsername" type = "text" placeholder="document ID"
+                   value = {this.state.inputUsername} onChange = {(e) => this.changeHandler(e)}></input>
+            <input id = "inputUsername" type = "text" placeholder="receiver"></input>
+            <input id = "inputUsername" type = "text" placeholder="receiver"></input>
+            <button onClick = {this.handleSubmit}>Submit</button>
             </div>
         )
     }
