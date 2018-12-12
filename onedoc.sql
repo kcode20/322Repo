b4387d45b5671ea131bca5d8568b661870d5eb8e
connie-code <conniewu06@gmail.com>
2018-12-11 23:17:04 -0500

    Updated and Revised the database

--- onedoc.sql
+++ onedoc.sql
@@ -7,9 +7,10 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  username varchar(15) NOT NULL,
-  email varchar(50) NOT NULL,
-  password char(60) NOT NULL
+  username varchar(15) NOT NULL UNIQUE,
+  email varchar(50) NOT NULL UNIQUE,
+  password char(60) NOT NULL,
+  type ENUM('G', 'OU', 'SU') NOT NULL DEFAULT 'G'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES users WRITE;
@@ -22,10 +23,10 @@ CREATE TABLE documents (
   docID int(11) NOT NULL AUTO_INCREMENT,
   owner int(11) NOT NULL,
   title varchar(45) NOT NULL,
-  content text,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  modified_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  locked ENUM('locked', 'unlocked') NOT NULL,
+  content blob,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  locked ENUM('locked', 'unlocked') NOT NULL DEFAULT 'locked',
   PRIMARY KEY (docID),
   KEY owner (owner),
   FOREIGN KEY (owner) REFERENCES users (id)
@@ -41,7 +42,7 @@ CREATE TABLE permissions (
   classID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   class tinytext NOT NULL,
   wrt bit(1) NOT NULL,
-  R bit(1) NOT NULL,
+  R BOOLEAN NOT NULL,
   complain bit(1) NOT NULL,
   new bit(1) NOT NULL,
   share bit(1) NOT NULL,
@@ -112,12 +113,45 @@ CREATE TABLE revisions (
 LOCK TABLES revisions WRITE;
 UNLOCK TABLES;
 
+--
+DROP TABLE IF EXISTS taboo;
 
--- Populate
+CREATE TABLE taboo(
+  tabooId INT PRIMARY KEY AUTO_INCREMENT,
+  tabooWord VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES taboo WRITE;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS invitations;
+
+CREATE TABLE invitations(
+  inviteID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  sender VARCHAR(255),
+  docID int(11) DEFAULT NULL,
+  receiver VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES invitations WRITE;
+UNLOCK TABLES;
+
+-- Populate database
+INSERT INTO taboo(tabooWord)
+VALUES ("fuck"),
+  ("shit"),
+  ("fucker"),
+  ("retard"),
+  ("idiot"),
+  ("dumbass");
+
 INSERT INTO users(username, email, password)
 VALUES ("Connie", "connie@gmail.com", "people123"),
-  ("Friend", "friend@gmail.com", "people123");
+  ("Khristian", "khristian@gmail.com", "people123"),
+  ("Song", "song@gmail.com", "people123"),
+  ("Chantelle", "chantelle@gmail.com", "people123");
 
-INSERT INTO documents(owner, title, content, locked) VALUES('1', 'My First Document', 'This is my first document. I love to write!', 'unlocked');
-INSERT INTO documents(owner, title, content, locked) VALUES('1', 'My Second Document', 'This is my second document. I like to write!', 'unlocked');
-INSERT INTO documents(owner, title, content, locked) VALUES('1', 'My Third Document', 'This is my third document. I hate to write!', 'unlocked');
+INSERT INTO documents(owner, title, content, created_at, modified_at, locked)
+VALUES (1, "Software Engineering", "asdfghjkl;qwertyuiopzxcvbnm", DATE '2018-08-15', DATE '2018-10-15', 'locked'),
+ (1, "Flower", "Tulips, daisy, sunflower", DATE '2018-11-15', DATE '2018-12-01', 'locked'),
+ (2, "US history", "American Revolution, George Washington", DATE '2018-12-02', DATE '2018-12-10', 'locked');
